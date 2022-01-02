f39d84feb1eca3dff40540050eba3a536de35d3b
nizam754 <m.nizam754@gmail.com>
2022-01-02 12:22:34 +0600

    Header Section

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+#------Header Section------------
+
+st.subheader("Hi, I am Nizam :wave:")
+st.title("A IT Specialist From Bangladesh")
+st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
+st.write("[Learn More >](https://www.google.com)")
