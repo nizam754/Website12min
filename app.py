02a21debe553e63c08a6b1f389b5d9f8c2307fa5
nizam754 <m.nizam754@gmail.com>
2022-01-02 17:52:04 +0600

    About Me Section

--- app.py
+++ app.py
@@ -4,8 +4,27 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 #------Header Section------------
+with st.container():
+    st.subheader("Hi, I am Nizam :wave:")
+    st.title("A IT Specialist From Bangladesh")
+    st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
+    st.write("[Learn More >](https://www.google.com)")
 
-st.subheader("Hi, I am Nizam :wave:")
-st.title("A IT Specialist From Bangladesh")
-st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
-st.write("[Learn More >](https://www.google.com)")
+#----What I Do-------------
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        st.write("[youTube Channel >](https://www.youtube.com)")
