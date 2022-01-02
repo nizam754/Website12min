f458c2b91efcb934f066be2ec425a5ea45372aa7
nizam754 <m.nizam754@gmail.com>
2022-01-02 20:14:31 +0600

    Contact Form

--- app.py
+++ app.py
@@ -6,25 +6,36 @@ from streamlit_lottie import st_lottie
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
+
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
+
+# Use Local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 img_contact_form = Image.open("images/yt_contact_form.png")
 img_lottie_animation = Image.open("images/yt_lottie_animation.png")
 
-#------Header Section------------
+# ------Header Section------------
 with st.container():
     st.subheader("Hi, I am Nizam :wave:")
     st.title("A IT Specialist From Bangladesh")
-    st.write("I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
+    st.write(
+        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings.")
     st.write("[Learn More >](https://www.google.com)")
 
-#----What I Do-------------
+# ----What I Do-------------
 with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
@@ -45,7 +56,7 @@ with st.container():
     with right_column:
         st_lottie(lottie_coding, height=300, key="coding")
 
-#----Projects----
+# ----Projects----
 with st.container():
     st.write("---")
     st.header("My Projects")
@@ -76,3 +87,25 @@ with st.container():
                 """
             )
             st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
+
+# -----Contact------
+with st.container():
+    st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
+    # Documentation: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/nizam.necit@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here" required></textarea>
+     <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
